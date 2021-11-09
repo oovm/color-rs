f2fed34c34d13be87f628a832080e2853a68dbbb
Aster <galaster@foxmail.com>
2021-11-09 16:08:09 +0800

    🔮 Add named colors

---
+++ projects/color/src/rgb/rgb/named_colors.m
@@ -0,0 +1,32 @@
+(* ::Package:: *)
+
+SetDirectory@NotebookDirectory[];
+
+
+getConst[name_->color_]:=Block[
+{r,g,b},
+{r,g,b}=Round[255*List@@RGBColor[color]];
+TemplateApply["
+    /// <span style=\"color:`name1`\">`name1`: `c`</span>
+    pub const `name2`: Self = Self { r: `r`, g: `g`, b: `b` };
+",<|
+"name1" -> name,
+"name2" -> ToUpperCase@name,
+"c" ->  ToUpperCase@color,
+"r" -> r,
+"g" -> g,
+"b" -> b
+|>]
+]
+
+
+rs = TemplateApply["\
+use super::*;
+
+impl RGB {
+`fs`
+}
+",<|
+"fs" -> StringRiffle[getConst/@Import["named_colors.json"],""]
+|>];
+Export["named_colors.rs",rs, "Text"]
