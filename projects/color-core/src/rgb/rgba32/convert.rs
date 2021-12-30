0a079c0d008878203c81d45818921cb83a995e12
Aster <galaster@foxmail.com>
2021-12-30 23:15:13 +0000

    🐣 Add rand support for rgb

--- projects/color-core/src/rgb/rgba32/convert.rs
+++ projects/color-core/src/rgb/rgba32/convert.rs
@@ -1,8 +1,12 @@
 use super::*;
 
-impl From<&RGBA32> for RGBA32 {
-    fn from(rgba: &RGBA32) -> Self {
-        *rgba
+impl<T> From<&T> for RGBA32
+where
+    Self: From<<T as ToOwned>::Owned>,
+    T: ToOwned,
+{
+    fn from(rgba: &T) -> Self {
+        rgba.to_owned().into()
     }
 }
 
@@ -15,5 +19,17 @@ impl From<RGB> for RGBA32 {
 impl From<RGBA> for RGBA32 {
     fn from(rgba: RGBA) -> Self {
         Self { r: rgba.r as f32 / 255.0, g: rgba.g as f32 / 255.0, b: rgba.b as f32 / 255.0, a: rgba.a as f32 / 255.0 }
+    }
+}
+
+impl<R, G, B, A> From<(R, G, B, A)> for RGBA32
+where
+    R: Into<f32>,
+    G: Into<f32>,
+    B: Into<f32>,
+    A: Into<f32>,
+{
+    fn from(rgba: (R, G, B, A)) -> Self {
+        Self { r: rgba.0.into(), g: rgba.1.into(), b: rgba.2.into(), a: rgba.3.into() }
     }
 }
