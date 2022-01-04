4c1cef57eca3e7be898844ed9d7ed06119960c2c
Aster <galaster@foxmail.com>
2022-01-04 17:21:12 +0000

    🔧 Fix display format of hsla

--- projects/color-core/src/hsl/hsla32/traits.rs
+++ projects/color-core/src/hsl/hsla32/traits.rs
@@ -10,8 +10,8 @@ impl Display for HSLA32 {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         let HSLA32 { h, s, l, a } = self.normalized();
         match f.alternate() {
-            true => write!(f, "hsla({:.0} {:.0}% {:.0}% / {:.0}%)", h * 360.0, s * 100.0, l * 100.0, a * 100.0),
-            false => write!(f, "hsla({} {}% {}% / {}%)", h * 360.0, s * 100.0, l * 100.0, a * 100.0),
+            true => write!(f, "hsla({} {}% {}% / {}%)", h * 360.0, s * 100.0, l * 100.0, a * 100.0),
+            false => write!(f, "hsla({:.0} {:.0}% {:.0}% / {:.0}%)", h * 360.0, s * 100.0, l * 100.0, a * 100.0),
         }
     }
 }
--- projects/color-core/tests/rgb/mod.rs
+++ projects/color-core/tests/rgb/mod.rs
@@ -1,12 +1,23 @@
 use color_core::{HSLA32, RGBA32};
 
 #[test]
-fn from_rgb() {
+fn from_rgb_1() {
     let hsla = HSLA32::new(0.2, 0.3, 0.4, 0.5);
+    assert_eq!(format!("{}", hsla), "hsla(72 30% 40% / 50%)");
 
     let rgba: RGBA32 = hsla.into();
-    assert_eq!(format!("{}", hsla), "hsla(72 30.000002% 40% / 50%)");
     assert_eq!(format!("{}", rgba), "rgba(120 133 71 / 50%)");
-    assert_eq!(format!("{:#}", rgba), "rgba(120 133 71 / 50%)");
-    assert_eq!(format!("{:X}", rgba), "rgba(0, 0, 0, 0.5)");
+    assert_eq!(format!("{:#}", rgba), "rgba(120.36 132.59999 71.4 / 50%)");
+    assert_eq!(format!("{:#X}", rgba), "#7884477F");
+}
+
+#[test]
+fn from_rgb_2() {
+    let hsla = HSLA32::new(0.0, 0.25, 0.5, 0.75);
+    assert_eq!(format!("{}", hsla), "hsla(0 25% 50% / 75%)");
+
+    let rgba: RGBA32 = hsla.into();
+    assert_eq!(format!("{}", rgba), "rgba(159 96 96 / 75%)");
+    assert_eq!(format!("{:#}", rgba), "rgba(159.375 95.625 95.625 / 75%)");
+    assert_eq!(format!("{:#X}", rgba), "#9F5F5FBF");
 }
