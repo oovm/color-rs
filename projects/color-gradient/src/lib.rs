562f835e12e2a71d64013cd8d9325cc3cfb8b269
Aster <aster@vers.site>
2022-02-07 01:08:50 +0000

    🐤 Add color gradient

---
+++ projects/color-gradient/src/lib.rs
@@ -0,0 +1,13 @@
+// #![forbid(missing_docs)]
+// #![forbid(missing_crate_level_docs)]
+// #![forbid(missing_debug_implementations)]
+// #![forbid(missing_doc_code_examples)]
+// #![doc = include_str!("../readme.md")]
+//
+// pub use color_core::*;
+// pub use color_macro::*;
+
+
+pub struct ColorGradient {
+
+}
---
+++ projects/color-gradient/tests/main.rs
@@ -0,0 +1,18 @@
+// use color_core::{RGB, RGBA};
+// use color_macro::{rgb, rgba};
+//
+// #[test]
+// fn test_rgb() {
+//     assert_eq!(rgb!("#346"), RGB::new(51, 68, 102));
+//     assert_eq!(rgb!("#334C66"), RGB::new(51, 76, 102));
+//     assert_eq!(rgb!("rgb(51, 76, 102)"), RGB::new(51, 76, 102));
+//     assert_eq!(rgb!("rgb(20% 30% 40%)"), RGB::new(51, 76, 102));
+// }
+//
+// #[test]
+// fn test_rgba() {
+//     assert_eq!(rgba!("#3467"), RGBA::new(51, 68, 102, 119));
+//     assert_eq!(rgba!("#334C667F"), RGBA::new(51, 76, 102, 127));
+//     assert_eq!(rgba!("rgba(51, 76, 102, .5)"), RGBA::new(51, 76, 102, 127));
+//     assert_eq!(rgba!("rgba(20% 30% 40% 50%)"), RGBA::new(51, 76, 102, 127));
+// }
