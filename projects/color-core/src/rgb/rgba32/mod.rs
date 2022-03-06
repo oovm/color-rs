c9aebdb8199285504dca3648ee3a2ce7de64f89c
Aster <galaster@foxmail.com>
2022-03-06 21:29:31 +0800

    Add map channel operator

--- projects/color-core/src/rgb/rgba32/mod.rs
+++ projects/color-core/src/rgb/rgba32/mod.rs
@@ -4,7 +4,7 @@ mod convert;
 mod traits;
 
 impl RGBA32 {
-    /// Normalize [`RGBA32`] color_parser.
+    /// Normalize [`RGBA32`] color.
     pub fn normalized(&self) -> Self {
         Self {
             r: self.r.min(1.0).max(0.0),
@@ -24,5 +24,26 @@ impl RGBA32 {
     /// Create [`RGBA32`] from `(u8, u8, u8, u8)` tuple.
     pub fn rgba(r: u8, g: u8, b: u8, a: u8) -> Self {
         RGBA { r, g, b, a }.into()
+    }
+    /// Map operator to r, g, b channels, without alpha channel.
+    pub fn map<F>(&self, f: F) -> Self
+    where
+        F: Fn(f32) -> f32,
+    {
+        Self { r: f(self.r), g: f(self.g), b: f(self.b), a: self.a }
+    }
+    /// Map operator to alpha channel only.
+    pub fn map_alpha<F>(&self, f: F) -> Self
+    where
+        F: Fn(f32) -> f32,
+    {
+        Self { r: self.r, g: self.g, b: self.b, a: f(self.a) }
+    }
+    /// Map operator to all channels.
+    pub fn map_all<F>(&self, f: F) -> Self
+    where
+        F: Fn(f32) -> f32,
+    {
+        Self { r: f(self.r), g: f(self.g), b: f(self.b), a: f(self.a) }
     }
 }
