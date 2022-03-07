ffe13f4447cf048c8ef6a502343e8f4a448874f5
Aster <galaster@foxmail.com>
2022-03-07 14:54:07 +0800

    Add support for lch

--- projects/color-core/src/lch/lcha32/traits.rs
+++ projects/color-core/src/lch/lcha32/traits.rs
@@ -1,4 +1,5 @@
 use std::fmt::{Display, Formatter};
+
 use super::*;
 
 impl Default for LCHA32 {
@@ -11,8 +12,8 @@ impl Display for LCHA32 {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         let LCHA32 { l, c, h, a } = self.normalized();
         match f.alternate() {
-            true => write!(f, "lch({} {}% {}% / {}%)", l * 360.0, c * 100.0, h * 360.0, a * 100.0),
-            false => write!(f, "lch({:.0} {:.0}% {:.0}% / {:.0}%)", l * 360.0, c * 360.0, h * 100.0, a * 100.0),
+            true => write!(f, "lch({}% {} {} / {}%)", l * 100.0, c, h * 360.0, a * 100.0),
+            false => write!(f, "lch({:.0}% {:.0} {:.0} / {:.0}%)", l * 100.0, c, h * 100.0, a * 100.0),
         }
     }
 }
